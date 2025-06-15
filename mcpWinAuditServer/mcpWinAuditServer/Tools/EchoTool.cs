649721095e20c4ab55f8cca689ff430e07b88995
Coder <temp@no.reply.com>
2025-06-16 02:23:38 +0300

    feat(ProcessTool): Implement ListAllProcesses to list all system processes

--- mcpWinAuditServer/mcpWinAuditServer/Tools/EchoTool.cs
+++ mcpWinAuditServer/mcpWinAuditServer/Tools/EchoTool.cs
@@ -1,11 +1,22 @@
 using System.ComponentModel;
 using System.Threading.Tasks;
 using ModelContextProtocol.Server;
+using System.Diagnostics;
+using System.Linq;
 
 namespace mcpWinAuditServer.Tools {
 [McpServerToolType]
-public static class EchoTool {
-    [McpServerTool, Description ( "Echoes the message back to the client." )]
-    public static string Echo ( string message ) => $"hello {message}";
+public static class ProcessTool {
+    [McpServerTool, Description ( "Lists all running processes on the system." )]
+    public static Task<object> ListAllProcesses() {
+        var processes = Process.GetProcesses().Select(p => new 
+        {
+            Id = p.Id,
+            ProcessName = p.ProcessName,
+            MainWindowTitle = p.MainWindowTitle,
+            Responding = p.Responding
+        }).ToList();
+        return Task.FromResult<object>(processes);
+    }
 }
 }
